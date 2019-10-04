d9289b4b4584ac943c7b5a51ec9832bb61b97e6b
Eduard Bloch <blade@debian.org>
2019-10-04 14:18:14 +0200

    Minimal apk flashing

--- nanares.py
+++ nanares.py
@@ -5,8 +5,10 @@
 import sys
 import os
 import subprocess
+from subprocess import Popen, DEVNULL, PIPE
 import errno
 import dialog
+from dialog import Dialog
 import argparse
 import io
 import re
@@ -24,12 +26,6 @@ no_cult_env = {"LANG": "C.UTF-8", "LC_ALL": "C.UTF-8"}
 dlgwidth = 60
 dlgheight= 20
 err_to_ignore = [ "tar: Malformed extended header: missing equal sign", "tar: Removing leading `/' from member names", "tar: Exiting with failure status due to previous errors" ]
-
-try:
-    (cols, lins) = os.get_terminal_size()
-    dlgwidth=int(cols*4/5)
-    dlgheight=int(lins*4/5)
-except: pass
 
 def die(code, msg):
     sys.stderr.write(msg)
@@ -67,8 +63,15 @@ except:
     die(1, "Error: $HOME not set, use --wd to set work directory")
 parser.add_argument('--wd', help="Work directory, default: " + wd_default, default = wd_default)
 parser.add_argument('--tmp', help="If specified, take this as folder with extracted data and ignore --wd")
+parser.add_argument('--nodlg', action='store_true', help="No dialog interaction, scan only")
 parser.add_argument('src', nargs='?', help="Source directory containing a single TWRP backup set")
 args = parser.parse_args()
+
+try:
+    (cols, lins) = os.get_terminal_size()
+    dlgwidth=int(cols*4/5)
+    dlgheight=int(lins*4/5)
+except: args.nodlg = True
 
 dlg = dialog.Dialog()
 tmp_dir = os.path.join(args.wd, "tmp")
@@ -163,43 +166,107 @@ for (x,y,z) in os.walk(os.path.join(tmp_dir, "data", "app")):
         apps[pkg]={"name": name, "apkpath": apkpath, 'sel': False}
 
 if not apps: die(21, "Failed to find any valid apps in " + tmp_dir)
-key_apps_data = "APPS+DATA"
+#key_apps_data = "APPS+DATA"
 key_apps="APPS"
 key_data="DATA"
+key_uninstall="DEINST"
 key_sel="SELECT_APPS"
 key_sel_all="SELECT_ALL"
 key_unsel_all="CLEAR_SELECTION"
 key_sel_load="LOAD_SELECTION"
 
-choices = [
+choices_main = [
     (key_sel, "Select apps"),
     (key_sel_all, "Check everything"),
     (key_unsel_all, "Uncheck everything"),
     ("===", "========"),
-    (key_apps_data, "Inject apps and data"),
-    (key_apps, "Install only APKs"),
-    (key_data, "Install only data"),
+    #(key_apps_data, "Inject apps and data"),
+    (key_apps, "Install APKs"),
+    (key_data, "Install app data"),
+    (key_uninstall, "Uninstall selected apps if possible"),
     ("~~~", "~~~~~~~~"),
     (key_sel_load, "Load previous selection from a previous run in the same workdir")
     ]
 
+key_opt_default = "DEFAULT"
+key_opt_reinst = "REINSTALL_SAME"
+
+choices_options = [
+    (key_opt_default, "Attempt apk installation, then data, on failure: back off", True),
+    (key_opt_reinst, "Try apk reinstallation", True)
+    ]
+
 sel_ser_file = os.path.join(args.wd, 'selection.py')
 
-def apply_selection(sel):
+def apply_selection(sel, invert_selection=False):
     global apps
     check_all = len(sel) == 1 and sel[0] == '*'
     for el in apps:
         if check_all: apps[el]['sel'] = True
-        else: apps[el]['sel'] = (el in sel)
+        else: apps[el]['sel'] = (el in sel) != invert_selection
     with open(sel_ser_file, 'w') as f:f.write(repr(sel))
 
+def confirm_start():
+    instructions = "Please check your phone and make sure that the phone is in offline mode (flight/plane mode) and that all active apps are closed (double check the app list!)" \
+"For some phones, turning off the display/lockscreen timeout is also a good idea. When ready, press OK."
+    return dialog.Dialog.OK == dlg.yesno(instructions, width=dlgwidth)
+
+#def selected_appsx():
+#    for el in apps:
+#        if(apps[el]["sel"]): yield el
+
 def flash_selection(mode):
+    selected_apps = filter(lambda el: apps[el]["sel"], apps)
+    if mode == key_apps: # or mode == key_apps_data:
+        for el in selected_apps:
+            print(el) 
     return True
+
+def selected_apps():
+    return list(filter(lambda key: apps[key]["sel"], apps))
+
+key_sel_unsuc = "Uncheck_Succeeded"
+key_sel_unfail= "Uncheck_Failed"
+key_log = "View_Log"
+#log_file = os.path.join(
+
+def post_dialog(log, suc_sel):
+    if len(suc_sel) == len(selected_apps()):
+        msg = "Congratulations, {0} selected operations succeeded".format(len(suc_sel))
+    else:
+        msg = "Some of the selected operations succeeded ({0} of {1})".format(len(suc_sel), len(selected_apps()))
+    dlg.msgbox(log)
+    exit(1)
+
+def install_apks():
+    logbuf = ''
+    good = []
+    for key in selected_apps():
+        msg = f"NANARES: Installing {key}:\n"
+        logbuf += msg
+        p = Popen(["adb", "install", "-r", apps[key]["apkpath"]], stdout=PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        sout, serr = p.communicate()
+        if p.wait() == 0: good.append(key)
+        for s in sout: logbuf += s
+    post_dialog(logbuf, good)
+
+if args.nodlg:
+    print("No terminal capabilities, aborting interactive mode")
+    exit(11)
+
+while True:
+    p = Popen(["adb", "shell", 'su -c "ls /data/data"'], stdout=DEVNULL, stderr=DEVNULL)
+    if p.wait() == 0: break
+    q = dlg.yesno("Could not execute a root mode command.\n\n"
+            + "Please check the phone, make sure that ADB is enabled in Development Settings and that Root manager allows su command for sufficiently long time (like an hour)\n\n"
+            + "Check again?",
+            width = dlgwidth)
+    if Dialog.OK != q: exit(9)
 
 while(True):
     count_all = len(apps)
     count_sel = len(list(filter(lambda el: apps[el]['sel'], apps)))
-    choice, resmode = dlg.menu(f"Please select packages and/or what to do with them. Currently selected: {count_sel}/{count_all}", choices = choices, height=dlgheight, width=dlgwidth, menu_height=dlgheight-5)
+    choice, resmode = dlg.menu(f"Please select packages and/or what to do with them. Currently selected: {count_sel}/{count_all}", choices = choices_main, height=dlgheight, width=dlgwidth, menu_height=dlgheight-5)
     if choice != dialog.Dialog.OK: exit(0)
     if resmode == key_sel_all:
         apply_selection(['*'])
@@ -214,8 +281,10 @@ while(True):
         #print(appchoices)
         ret_btn, ret_items = dlg.checklist("Apps to act on:", choices = appchoices, width=dlgwidth, height=dlgheight, list_height=dlgheight-2)
         apply_selection(ret_items)
-    if resmode == key_apps or resmode == key_apps_data or resmode == key_data:
-        flash_selection(resmode)
+#    if resmode == key_apps or resmode == key_apps_data or resmode == key_data:
+#        flash_selection(resmode)
+    if resmode == key_apps:
+        install_apks()
     if resmode == key_sel_load:
         with open(sel_ser_file, 'r') as f: sel = ast.literal_eval(f.read())
         apply_selection(sel)
